bfe6fda29f43ea5bba907cdbb5c2a17d18eaf0a8
Dave <daveying@163.com>
2018-01-02 22:58:07 +0800

    first commit

---
+++ TPL/ParallelInvoke/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelInvoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(() => Console.WriteLine("Job 1"), () => Console.WriteLine("Job 2"), () => Console.WriteLine("Job 3"));
+            Console.Read();
+        }
+    }
+}
---
+++ TPL/Task/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskExample
+{
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+            Task taskA = new Task(() => Console.WriteLine("Hello from taskA"));
+            taskA.Start();
+            Console.WriteLine("Hello from thread '{0}'.", Thread.CurrentThread.Name);
+            taskA.Wait();
+        }
+
+        static void Main3(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+            Task taskA = Task.Run(() => Console.WriteLine("Hello from taskA"));
+            Console.WriteLine("Hello from thread '{0}'.", Thread.CurrentThread.Name);
+            taskA.Wait();
+        }
+
+        static void Main4(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+            Task taskA = Task.Factory.StartNew(() => Console.WriteLine("Hello from taskA"));
+            Console.WriteLine("Hello from thread '{0}'.", Thread.CurrentThread.Name);
+
+            taskA.Wait();
+        }
+
+        static void Main(string[] args)
+        {
+            Task<Double>[] taskArray = {Task<Double>.Factory.StartNew(() => DoComputation(1.0)),
+                                        Task<Double>.Factory.StartNew(() => DoComputation(100.0)),
+                                        Task<Double>.Factory.StartNew(() => DoComputation(1000.0))};
+            var result = new Double[taskArray.Length];
+            Double sum = 0;
+
+            for (int i = 0; i < taskArray.Length; ++i)
+            {
+                result[i] = taskArray[i].Result;
+                Console.Write("{0:N1} {1}", result[i], i == taskArray.Length - 1 ? "= " : "+ ");
+                sum += result[i];
+            }
+            Console.WriteLine("{0:N1}", sum);
+
+        }
+
+        private static Double DoComputation(Double start)
+        {
+            Double sum = 0;
+            for (var value = start; value <= start + 10; value += .1)
+                sum += value;
+            return sum;
+        }
+    }
+}
